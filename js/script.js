d719c5a69d0ef54a51f234f340ec66f312925c95
Wraith1982 <rattlehead20@poczta.onet.pl>
2016-06-27 05:21:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ var allCategoriesUrl =
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl = 
-  "js/specials.json";
+  "http://davids-restaurant.herokuapp.com/menu_items.json?category=SP";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
