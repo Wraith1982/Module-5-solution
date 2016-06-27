b696d8c66d9389d483f7c5203a001c898f36132a
Wraith1982 <rattlehead20@poczta.onet.pl>
2016-06-27 17:01:01 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ var allCategoriesUrl =
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl = 
-  "js/specials.json";
+  "js/items.json";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
